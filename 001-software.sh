5099d9a429e0e5a78436e89361e134c9a7cdb832
Doug McClure <dmcclure@biznuvo.com>
2025-03-11 21:04:09 -0400

    Add firmware upgrade

--- 001-software.sh
+++ 001-software.sh
@@ -1,9 +1,13 @@
 printf "\n#### BEGIN CONFIG : Software\n\n"
 
+fwupdmgr -y --no-reboot-check upgrade
+
 dnf upgrade -y
 dnf install -y vim
 
 # https://discussion.fedoraproject.org/t/vim-default-editor-in-coreos/71356/4
 dnf swap -y nano-default-editor vim-default-editor --allowerasing
+
+touch /tmp/doreboot
 
 printf "\n#### FINISHED CONFIG : Software\n\n"
