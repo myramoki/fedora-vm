376fcbf0f1d40647cc2b52648753f90432f4b9fd
Doug McClure <dsm.mcclure@gmail.com>
2025-08-08 12:07:29 -0400

    Update config data

--- 201-github-ssh.sh
+++ 201-github-ssh.sh
@@ -5,17 +5,8 @@ printf "#- fetch ssh keys\n"
 mkdir -p $SUDO_USER_HOME/.ssh
 
 sshkeystempfile=$(mktemp /tmp/tmp.dl.sshkeys.XXXXXXXXXX)
-curl -sL -o $sshkeystempfile $GITDIR/biznuvo-server-keys.tar.xz.gpg
+curl -sL -o $sshkeystempfile $GITDIR/ssh-config.tar.xz.gpg
 gpg -d $sshkeystempfile | tar -J -xvf - -C $SUDO_USER_HOME/.ssh
-
-printf "#- configure ssh config\n"
-
-printf "
-Host github.com
-    Hostname ssh.github.com
-    Port 443
-    IdentityFile=~/.ssh/biznuvo-server-v2-id_ed25519
-" >> $SUDO_USER_HOME/.ssh/config
 
 chown -R $SUDO_USER: $SUDO_USER_HOME/.ssh
 
