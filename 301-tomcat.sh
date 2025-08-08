7d4699d1d83fb5ecd613a56bfcb19a0e221dafa2
Doug McClure <dsm.mcclure@gmail.com>
2025-08-08 12:38:06 -0400

    Update tomcat build and scripts

--- 301-tomcat.sh
+++ 301-tomcat.sh
@@ -43,11 +43,10 @@ chown -R tomcat:tomcat /opt/tomcat/
 chmod -R u+x /opt/tomcat/bin
 chmod -R go+rX /opt/tomcat
 
-cd /opt/tomcat/.local/bin
-sudo -u tomcat curl -O $GITDIR/scripts/tomcat/updatedes -O $GITDIR/scripts/tomcat/updateops
-sudo -u tomcat chmod +x /opt/tomcat/.local/bin/*
-
 cd /opt/tomcat
+sudo -u tomcat curl -O $GITDIR/scripts/tomcat/update-app.sh
+sudo -u tomcat chmod +x /opt/tomcat/update-app.sh
+
 sudo -u tomcat git clone https://github.com/myramoki/fedora-dotfiles.git .dotfiles
 
 cd /opt/tomcat/.dotfiles
